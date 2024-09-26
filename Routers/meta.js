48f48b07b141e2eb000f2975e90e4aec1dcb6848
mohammadashrafulislam <ixdell41@gmail.com>
2024-09-26 12:05:18 +0600

    fix the error

--- Routers/meta.js
+++ Routers/meta.js
@@ -7,10 +7,10 @@ import { verifyEditor } from "../MiddleWare/jwt.js";
 export const metaRouter = express.Router();
 
 // Route to add new footer meta
-metaRouter.post("/add", verifyEditor(), upload.single("FeaturedImage"), addMeta);
+metaRouter.post("/add", verifyEditor, upload.single("FeaturedImage"), addMeta);
 
 // Route to update footer meta by ID
-metaRouter.put("/update/:id", verifyEditor(), upload.single("FeaturedImage"), updateMeta);
+metaRouter.put("/update/:id", verifyEditor, upload.single("FeaturedImage"), updateMeta);
 
 // Route to get all footer meta
 metaRouter.get("/", getAllMeta);
