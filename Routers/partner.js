21519faa6349d32824406c28a6512a0d62526dcf
mohammadashrafulislam1 <mohammadashrafulislam33@gmail.com>
2024-10-02 14:16:26 +0600

    updated

--- Routers/partner.js
+++ Routers/partner.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { addPartner, deletePartner, getPartners } from "../Controllers/PartnerController.js";
 import { verifyAdmin, verifyEditor } from "../MiddleWare/jwt.js";
+import { upload } from "../MiddleWare/multer.js";
 
 export const partnerRouter = express.Router();
 
